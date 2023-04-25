9f86cea0960f5d2e39a2b00c5f039c168939dbf2
ViniciusAnverze <viniverze@gmail.com.br>
2023-04-25 17:32:21 -0300

    retirei algumas constantes desnecessarias

--- Gender-and-Age-Detection-master/main.py
+++ Gender-and-Age-Detection-master/main.py
@@ -11,10 +11,6 @@ HORA = LISTA_HORA[0]
 CAMINHO_HORA = f'//192.168.1.136/apps/fotos/{DATA}/{HORA}'
 CAMINHO_DATA = f'//192.168.1.136/apps/fotos/{DATA}'
 
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-AGE_LIST = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-GENDER_LIST = ['Masculino', 'Feminino']
-
 contadorMasculino, contadorFeminino, faixaEtariaFinal = verificarTodasFotos(LISTA_FOTOS, DATA, HORA)
 
 printarInformacoes(DATA, HORA, contadorMasculino, contadorFeminino, faixaEtariaFinal)
